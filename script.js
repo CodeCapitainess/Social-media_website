628d4722d560082844a26fbe578d65c92a033698
CodeCapitainess <masailimedveda73@gmail.com>
2024-03-23 18:54:52 +0400

    Social-media_website

--- script.js
+++ script.js
@@ -79,30 +79,43 @@ themeModal.addEventListener('click', openThemeModal);
 theme.addEventListener('click', openThemeModal);
 
 // =========== Fonts ========= //
-fontSizes.forEach(size => {
-    let fontSize;
-    if(size.classList.contains('font-size-1')){
-        fontSize = '10px';
-        root.style.setProperty('----sticky-top-left', '5.4rem');
-        root.style.setProperty('----sticky-top-right', '5.4rem');
-    } else if(size.classList.contains('font-size-2')){
-        fontSize = '13px';
-        root.style.setProperty('----sticky-top-left', '5.4rem');
-        root.style.setProperty('----sticky-top-right', '-7rem');
-    } else if(size.classList.contains('font-size-3')){
-        fontSize = '16px';
-        root.style.setProperty('----sticky-top-left', '-2rem');
-        root.style.setProperty('----sticky-top-right', '-17rem');
-    } else if(size.classList.contains('font-size-4')){
-        fontSize = '19px';
-        root.style.setProperty('----sticky-top-left', '-5rem');
-        root.style.setProperty('----sticky-top-right', '-25rem');
-    } else if(size.classList.contains('font-size-5')){
-        fontSize = '22px';
-        root.style.setProperty('----sticky-top-left', '-10rem');
-        root.style.setProperty('----sticky-top-right', '5.4rem');
-    }
+//remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
 
+fontSizes.forEach(size => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle('active');
+
+    size.addEventListener('click', () => {
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        } else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-10rem');
+            root.style.setProperty('----sticky-top-right', '-33rem');
+        }
+
+        
     // change font size of the root html element
     document.querySelector('html').style.fontSize = fontSize;
+    })
 })
