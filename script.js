47719c480821fdfe36666ae906f181524bf6b716
CodeCapitainess <masailimedveda73@gmail.com>
2024-03-23 18:48:04 +0400

    Social-media_website

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const messageSearch = document.querySelector('#message-search');
 const theme = document.querySelector('#theme'); 
 const themeModal = document.querySelector('.customize-theme'); 
 const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
 
 // ============= Sidebar ============ //
 
@@ -82,13 +83,26 @@ fontSizes.forEach(size => {
     let fontSize;
     if(size.classList.contains('font-size-1')){
         fontSize = '10px';
+        root.style.setProperty('----sticky-top-left', '5.4rem');
+        root.style.setProperty('----sticky-top-right', '5.4rem');
     } else if(size.classList.contains('font-size-2')){
         fontSize = '13px';
+        root.style.setProperty('----sticky-top-left', '5.4rem');
+        root.style.setProperty('----sticky-top-right', '-7rem');
     } else if(size.classList.contains('font-size-3')){
         fontSize = '16px';
+        root.style.setProperty('----sticky-top-left', '-2rem');
+        root.style.setProperty('----sticky-top-right', '-17rem');
     } else if(size.classList.contains('font-size-4')){
         fontSize = '19px';
+        root.style.setProperty('----sticky-top-left', '-5rem');
+        root.style.setProperty('----sticky-top-right', '-25rem');
     } else if(size.classList.contains('font-size-5')){
         fontSize = '22px';
+        root.style.setProperty('----sticky-top-left', '-10rem');
+        root.style.setProperty('----sticky-top-right', '5.4rem');
     }
+
+    // change font size of the root html element
+    document.querySelector('html').style.fontSize = fontSize;
 })
